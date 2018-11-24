7badb87398fb8815064e3d9b8f9d3f876dd0568e
YU JIN <dbwls7152@naver.com>
2018-11-24 04:32:52 -0800

    make text file using input value

--- main.c
+++ main.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define _CRT_SECURE_NO_WARNINGS
 
 int main()
 {
-	FILE *fp = fopen("./hello.txt", "w");
+ // input
+ char path[] = "./home/chaeyujin/sw_eng/project";
+ char name[100], std_num[20], ch;
+ char full_path[150];
+ char* p;
+ FILE *fp = NULL;
 
-	fprintf(fp, "%s %d\n", "Hello", 100);   
+ printf("Enter you name :\n"); 
+ fgets(name,100, stdin);
 
-	fclose(fp);    
+ printf("Enter your student number :\n"); 
+ fgets(std_num,20,stdin);
 
-	return 0;
+
+
+ //remove enter key
+
+ if((p = strchr(name, '\n'))!= NULL)   *p = '\0';
+ if((p = strchr(std_num, '\n'))!= NULL)   *p = '\0';
+
+
+
+ // save in the text file.
+ sprintf(full_path,"%s%s",std_num,".txt");
+ printf("%s",full_path);
+
+ fp = fopen(full_path,"w");
+ if(fp != 0)
+ {
+  fprintf(fp, "%s %s\n", name,std_num);   
+  fclose(fp);
+ }
+ else
+  printf("fail\n");
+
+ return 0;
+
 }
