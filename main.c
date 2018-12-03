4c5a368ee265fc32ec5c12af76c5561b8bb34811
YU JIN <dbwls7152@naver.com>
2018-12-03 03:17:41 -0800

    subject data

--- main.c
+++ main.c
@@ -2,27 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define _CRT_SECURE_NO_WARNINGS
-
 int main()
 {
  // input
- char path[] = "./home/chaeyujin/sw_eng/project";
  char name[100], std_num[20], ch;
  char full_path[150];
  char* p;
  FILE *fp = NULL;
 
- printf("Enter you name :\n"); 
+ printf("Enter you NAME :\n"); 
  fgets(name,100, stdin);
 
- printf("Enter your student number :\n"); 
+ printf("Enter your STUDENT NUMBER :\n"); 
  fgets(std_num,20,stdin);
 
-
-
  //remove enter key
-
  if((p = strchr(name, '\n'))!= NULL)   *p = '\0';
  if((p = strchr(std_num, '\n'))!= NULL)   *p = '\0';
 
@@ -30,17 +24,13 @@ int main()
 
  // save in the text file.
  sprintf(full_path,"%s%s",std_num,".txt");
- printf("%s",full_path);
-
  fp = fopen(full_path,"w");
  if(fp != 0)
  {
   fprintf(fp, "%s %s\n", name,std_num);   
   fclose(fp);
  }
- else
-  printf("fail\n");
-
+ else  printf("fail\n");
+ 
  return 0;
-
 }
