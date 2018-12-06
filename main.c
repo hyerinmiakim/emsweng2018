dff7fa3b377163d66caa98ea8b406f9c57692d94
Hyerin Kim <hlkim266@gmail.com>
2018-12-05 22:50:59 -0800

    edit main

--- main.c
+++ main.c
@@ -8,13 +8,14 @@
 
 #include "funnel_a2.c"
 #include "printinfo.c"
+
 char(*funnel(char* _csvfile))[max];
-char(*get_the_info(char* _subject, char** _minor))[max];
+char(*get_the_info(char* sub, void *_minor))[max];
 
 int main()
 {
  // input
- char name[100], std_num[20], ch;
+ char name[100], std_num[20], grade = '\0', subj[50];
  char full_path[150];
  char* p;
 
@@ -28,30 +29,38 @@ int main()
  printf("Enter your STUDENT NUMBER :\n"); 
  fgets(std_num,20,stdin);
 
+ printf("Enter your subjects : \n");
+ fgets(subj,50,stdin);
+
+ //printf("Enter your grade : \n");
+ //fgets(&grade,2,stdin);
+
  //remove enter key
  if((p = strchr(name, '\n'))!= NULL)   *p = '\0';
  if((p = strchr(std_num, '\n'))!= NULL)   *p = '\0';
-
-
+ if((p = strchr(subj, '\n'))!= NULL)   *p = '\0';
+ //if(!(grade == '\n'))   *p = '\0';
 
  // save in the text file.
  sprintf(full_path,"%s%s",std_num,".txt");
  fp = fopen(full_path,"w");
  if(fp != 0)
  {
-  fprintf(fp, "%s %s\n", name,std_num);   
-  fprintf(fp, "%s %s", name,std_num);   
+  fprintf(fp, "%s %s %s\n", name,std_num, subj);   
+  //fprintf(fp, "%s %s", name,std_num);   
   fclose(fp);
  }
  else  printf("fail\n");
  
 
- // call the func named funnel 
- char(*arr)[max] = funnel("2018_ese.csv.0");
- //char(*info)[max] = get_the_info(NULL,arr);
+ // call the functions.
+  char *psubj = '\0';
+        psubj = subj;
+  char(*arr)[max] = funnel("2018_ese.csv.0");
+  char(*info2)[max] = get_the_info(psubj, &arr[0]);
 
- // for printing and handling arr: *arr[0][2]...
 
+ // for printing and handling arr: *arr[0][2], info[0][3]
 
  return 0;
 }
