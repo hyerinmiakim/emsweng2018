57193ac4bd5c38190523dd6d5c053504ce7fb253
Hyerin Kim <hlkim266@gmail.com>
2018-12-08 23:43:48 -0800

    handel error

--- graduate.c
+++ graduate.c
@@ -5,7 +5,7 @@ void graduate()
   printf("Course Completed Type: "); 
   for (i = 0; i < max; i++)
   {
-	cnt = cnt+info[4][i];
+	cnt = cnt+info[3][i];
 	printf("%c", info[0][i]);
   }
   printf(" and Credit is %d. \n", cnt);
